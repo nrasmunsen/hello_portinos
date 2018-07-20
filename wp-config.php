1854784d3e97de928a9b7e9054d9b4ef4f785f4c
portinos <intel.portinos@gmail.com>
2018-07-20 18:35:00 -0300

    Updated WP-Config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'agency_blog');
+define('DB_NAME', 'hello');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'forge');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'deQF9KJ88rs5wpATQ5WC');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
