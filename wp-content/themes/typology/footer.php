bc560d15cdf8c9c17485e3d6279f6311d8f479d9
portinos <intel.portinos@gmail.com>
2018-09-18 21:16:56 -0300

    Added GA Code

--- wp-content/themes/typology/footer.php
+++ wp-content/themes/typology/footer.php
@@ -19,6 +19,15 @@
 			</footer>
 
 		</div>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-340227-3"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-340227-3');
+		</script>
 
 		<?php get_sidebar(); ?>
 		
